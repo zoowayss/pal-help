73e0fd571a90aff68daad92172062482882c327b
zoowayss <shizuwei99@gmail.com>
2024-03-20 17:43:48 +0800

    feat: first commit

---
+++ src/main/java/top/help/pal/PalHelpApplication.java
@@ -0,0 +1,13 @@
+package top.help.pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/palhelp/PalHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.palhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
